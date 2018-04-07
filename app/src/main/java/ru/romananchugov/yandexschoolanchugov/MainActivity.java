424eaca1f2f539ce07bd67aeefc842ca2cb53e8f
RomanAnchugov <romagn@mail.ru>
2018-04-07 10:37:20 +0300

    first step with oauth

--- app/src/main/java/ru/romananchugov/yandexschoolanchugov/MainActivity.java
+++ app/src/main/java/ru/romananchugov/yandexschoolanchugov/MainActivity.java
@@ -1,13 +1,57 @@
 package ru.romananchugov.yandexschoolanchugov;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CLIENT_ID = "959666c7ee9942f6b9ffec283205e35c";
+    public static final String SECRET = "7e7dba7245b84dab88e6e4869dba4428";
+
+    public static final String AUTH_URL = "https://oauth.yandex.ru/authorize?response_type=token&client_id="+CLIENT_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startLogin();
+    }
+
+    public void startLogin(){
+        new AuthDialogFragment().show(getSupportFragmentManager(), "auth");
+    }
+
+    public static class AuthDialogFragment extends DialogFragment {
+
+        public AuthDialogFragment () {
+            super();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.example_auth_title)
+                    .setMessage(R.string.example_auth_message)
+                    .setPositiveButton(R.string.example_auth_positive_button, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick (DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(AUTH_URL)));
+                        }
+                    })
+                    .setNegativeButton(R.string.example_auth_negative_button, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick (DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            getActivity().finish();
+                        }
+                    })
+                    .create();
+        }
     }
 }
