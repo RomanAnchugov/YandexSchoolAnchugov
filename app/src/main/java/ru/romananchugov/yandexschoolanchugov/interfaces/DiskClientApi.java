c6aaf9b6d61791118a4eed2f275610be00857d40
RomanAnchugov <romagn@mail.ru>
2018-04-10 19:03:11 +0300

    first version of loading images

---
+++ app/src/main/java/ru/romananchugov/yandexschoolanchugov/interfaces/DiskClientApi.java
@@ -0,0 +1,15 @@
+package ru.romananchugov.yandexschoolanchugov.interfaces;
+
+import retrofit2.Call;
+import ru.romananchugov.yandexschoolanchugov.models.DowloadLink;
+
+/**
+ * Created by romananchugov on 09.04.2018.
+ */
+
+public interface GetDownloadLink {
+
+
+    Call<DowloadLink> 
+
+}
---
+++ app/src/main/java/ru/romananchugov/yandexschoolanchugov/models/DownloadLink.java
@@ -0,0 +1,23 @@
+package ru.romananchugov.yandexschoolanchugov.models;
+
+/**
+ * Created by romananchugov on 09.04.2018.
+ */
+
+public class DowloadLink {
+    private String href;
+    private String method;
+    private String templated;
+
+    public String getHref() {
+        return href;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getTemplated() {
+        return templated;
+    }
+}
